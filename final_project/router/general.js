caa79308eef6621ec66d8e30e56cb0dc8212de11
milanesd <thisisannoying@god.com>
2023-03-28 17:00:53 -0400

    finished task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,31 +13,76 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let filteredIsbns = Object.keys(books).filter((book) => book === isbn);
+  if (filteredIsbns.length > 0)
+  {
+      let foundIsbn = filteredIsbns[0];
+      let book = books[foundIsbn];
+      res.send(JSON.stringify(book)+'\n');
+  } else {
+      res.status(300).json({message: "Unable to find isbn: " + isbn});
+  }
+  return 
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let author = req.params.author;
+    let foundAuthors = [];
+    for(i in books) {
+        let book = books[i];
+        if ( author === book.author ) {
+            foundAuthors.push(book);
+        }
+    }
+    if (foundAuthors.length > 0) {
+        res.send(JSON.stringify(foundAuthors)+'\n');
+    } else {
+        
+        res.status(300).json({message: "Unable to find author: " + author});
+    }
+  return 
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let title = req.params.title;
+    let foundTitles = [];
+    for(i in books) {
+        let book = books[i];
+        if ( title === book.title ) {
+            foundTitles.push(book);
+        }
+    }
+    if (foundTitles.length > 0) {
+        res.send(JSON.stringify(foundTitles)+'\n');
+    } else {
+        
+        res.status(300).json({message: "Unable to find title: " + title});
+    }
+  return 
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn;
+    let filteredIsbns = Object.keys(books).filter((book) => book === isbn);
+    if (filteredIsbns.length > 0)
+    {
+        let foundIsbn = filteredIsbns[0];
+        let book = books[foundIsbn];
+        res.send(JSON.stringify(book.review)+'\n');
+    } else {
+        res.status(300).json({message: "Unable to find isbn: " + isbn});
+    }
+    return 
 });
 
 module.exports.general = public_users;
